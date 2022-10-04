bfadeffb51b6b405030a1fdc0e2cbf3bd3e6eab0
David Upegui <david@genial.io>
2022-10-04 17:57:40 -0500

    Added backend

--- ejemplos-terraform/provider.tf
+++ ejemplos-terraform/provider.tf
@@ -5,8 +5,15 @@ terraform {
       version = "~> 4.0"
     }
   }
+  backend "s3" {
+    bucket = "tf-state-david"
+    key = "remote-state"
+    region = "us-east-1"
+  }
 }
 
 provider "aws" { 
   region = "us-east-1"
 }
+
+
